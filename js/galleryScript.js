8dcd1c716a506b4891f46903d41feea11fd32d2a
Hardy173 <izvesten73@mail.ru>
2017-02-24 20:49:30 +0400

    24.02
    Доработки сайта

---
+++ js/galleryScript.js
@@ -0,0 +1,25 @@
+
+	$(document).ready(function() {
+		$("a.gallery1").fancybox({	
+			openEffect : 'elastic',
+			openSpeed  : 500,
+			closeEffect : 'elastic',
+			closeSpeed  : 500,
+			closeBtn  : true,
+			"padding" : 10,
+			"imageScale" : false, 
+			"zoomOpacity" : false,
+			"zoomSpeedIn" : 1000,	
+			"zoomSpeedOut" : 1000,	
+			"zoomSpeedChange" : 1000, 
+			"frameWidth" : 700,	 
+			"frameHeight" : 600, 
+			"overlayShow" : true, 
+			"overlayOpacity" : 0.8,	
+			"hideOnContentClick" :false,
+			"centerOnScroll" : false,
+			"showNavArrows" :	true
+		});
+	});
+
+	
